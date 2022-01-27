79907e447eca534c41637a4fcf9dbb5af166b402
desshub <desshub95@gmail.com>
2022-01-28 00:52:42 +0300

    Finalise on UI

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from "./router";
 import store from "./store";
 import axios from "axios";
 
-// axios.defaults.baseURL = "https://pokedexxapi.herokuapp.com/";
-axios.defaults.baseURL = "http://localhost:3030/";
+axios.defaults.baseURL = "https://pokedexxapi.herokuapp.com/";
 
 createApp(App).use(store).use(router).mount("#app");
